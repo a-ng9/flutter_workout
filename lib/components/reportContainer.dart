05f6046c6a0da8d1e1a269acda4bc90babe8fdbf
ND <aj.nglun@gmail.com>
2021-03-21 18:00:48 +0400

    Building a component

--- lib/components/reportContainer.dart
+++ lib/components/reportContainer.dart
@@ -3,39 +3,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter_workout/const.dart';
 
 class ReportContainer extends StatelessWidget {
+  const ReportContainer({Key key, this.margin, this.label, this.time})
+      : super(key: key);
+
+  final EdgeInsets margin;
+  final String label;
+  final String time;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      padding: const EdgeInsets.all(15),
-      height: 250,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(9))),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 30.0),
-            child: Icon(
-              Icons.timer,
-              color: darkBlack,
-              size: 35,
+    return Expanded(
+      child: Container(
+        margin: margin,
+        padding: const EdgeInsets.all(15),
+        height: 250,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(9))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30.0),
+              child: Icon(
+                Icons.timer,
+                color: darkBlack,
+                size: 35,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 40.0),
-            child: Text(
-              "Training Time",
-              style: TextStyle(fontSize: 20, color: darkBlack),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40.0),
+              child: Text(
+                label,
+                style: TextStyle(fontSize: 20, color: darkBlack),
+              ),
             ),
-          ),
-          Text(
-            "30 mins",
-            style: TextStyle(fontSize: 20, color: darkBlack),
-          )
-        ],
+            Row(
+              children: [
+                Text(time,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: darkBlack,
+                        fontWeight: FontWeight.w600)),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
